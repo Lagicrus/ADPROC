559c19fc4906ba63b5e9b364ca408ea041c520ce
lagicrus <Lagicrus@users.noreply.github.com>
2018-10-22 11:50:44 +0100

    WIP

---
+++ Week5/src/Main.java
@@ -0,0 +1,92 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner console = new Scanner(System.in);  //object of Scanner class
+
+    public static void main(String[] args) throws IOException {
+        String sName, again;
+        String sID;
+        ArrayList<Student> stud = new ArrayList<Student>();
+
+        int i, num = 0;
+        boolean answer;
+        int[] sMarks = new int[3];
+        String starting_scores;
+        String[] score_parts;
+
+        do {
+            System.out.println("\n Enter student first and last name, separated by space.");
+            sName = console.nextLine();   //returns (the rest of) the current input line
+
+            System.out.println("Enter three marks separated with whitespace!");
+            starting_scores = console.nextLine();
+            score_parts = starting_scores.split(" ");
+            System.out.println(starting_scores);
+            for (String score: score_parts){
+                if (this.isNumeric(score)){
+                    sMarks[score_parts.] = score;
+                }
+            }
+
+
+            for (i = 0; i < sMarks.length; i++) {
+                sMarks[i] = console.nextInt();    // scans the next token as an int
+            }
+            console.nextLine();         // to clear the rest of the current line
+
+            System.out.println("What is the students ID");
+            sID = console.nextLine();
+
+            Student current = new Student(sName, sID, sMarks);
+            stud.add(current);
+            System.out.println("\n Another student (y/n)?");
+            again = console.nextLine();         // this will get your answer to the above query
+
+            answer = false;            // Boolean variable for the interactive input
+
+            if (again.charAt(0) == 'y') {
+                answer = true;        // if data for another student is going to be entered
+                num++;        // increase the number of students
+            }
+        } while (answer);
+
+        Course cm = new Course("Computing", "CMP06");    // cm - first instance of class Course
+
+        for (i = 0; i < num / 2; i++) {
+            cm.addStudent(stud.get(i));
+        }
+
+        System.out.println("\n course name: " + cm.getName());
+        System.out.println("\t average: " + cm.averageSc());
+        System.out.println("\t number of students: " + cm.getNumStud());
+
+        for (i = 0; i < cm.getNumStud(); i++) {
+            cm.student_list[i].printOut();   // outputs the students’ info
+            System.out.println("\t average score:" + cm.student_list[i].avScore());
+        }
+
+        Course cs = new Course("Computer Science", "CS06"); // cs - second instance of class Course
+
+        for (i = num / 2; i < num; i++) // second half of the students will be added to cs
+        {
+            cs.addStudent(stud.get(i));
+        }
+
+        System.out.println("\n course name: " + cs.getName());
+        System.out.println("\t average: " + cs.averageSc());
+        System.out.println("\t number of students: " + cs.getNumStud());
+
+        for (i = 0; i < cs.getNumStud(); i++) {
+            cm.student_list[i].printOut();   // outputs the students’ info
+            System.out.println("\t average score:" + cm.student_list[i].avScore());
+        }
+    }
+
+    public boolean isNumeric(String number){
+        return number.matches("-?\\d+(\\.\\d+)?");
+    }
+
+}
---
+++ Week5/src/Student.java
@@ -0,0 +1,65 @@
+public class Student {
+    private String name;
+    private String id;
+    private int[] score = new int[3];
+
+
+    public Student() {
+    }
+
+    // Constructor, initialises the class instance variables and performs validity check
+
+    public Student(String stName, String stID, int stScore[]) {
+        name = stName;
+        id = stID;
+        score = stScore;
+    }
+
+    public void setName(String new_name) {
+        name = new_name;
+    }
+
+    public void setID(String student_ID) {
+        id = student_ID;
+    }
+
+    public void setScore(int student_Sc[]) {
+        for (int i = 0; i < 3; i++)
+            score[i] = student_Sc[i];
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public int[] getScore() {
+        return score;
+    }
+
+
+    public double avScore() {
+        double total = 0.0;
+        for (int i = 0; i < score.length; i++)
+
+            total += score[i];
+
+        return (total / score.length);
+
+    }
+
+    public void printOut() {
+        System.out.println("\n Student attributes: ");
+        System.out.println("\t name : " + name + "\n\t id : " + id);
+
+        for (int i = 0; i < score.length; i++)
+            System.out.print("\tscore [" + i + "] = " + score[i]);
+        System.out.println("\n\t average score = " + avScore());
+
+    }
+
+}
+
